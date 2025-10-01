0722a36a951196c3a09839b2199be85e3016683f
ironiegenie <82648379+ironiegenie@users.noreply.github.com>
2025-10-01 16:37:57 +0200

    Farbe funzt

--- src/app/components/random-card/random-card.ts
+++ src/app/components/random-card/random-card.ts
@@ -3,18 +3,22 @@ import { CallingPokeapi } from '../../services/calling-pokeapi';
 import { Randomizer } from '../../services/randomizer';
 import { KartenDaten } from '../../model/kartenDaten.type';
 import { KartenDatenBuilder } from '../../classes/karten-daten-builder';
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
   selector: 'app-random-card',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './random-card.html',
   styleUrl: './random-card.scss'
 })
 export class RandomCard implements OnInit {
   pokedexNumber: number = 0;
-  constructor(private randomizer: Randomizer, private callingPokeApi: CallingPokeapi) { };
   pokemon: KartenDaten = new KartenDatenBuilder().build();
   datenSignal = signal(this.pokemon);
+  akzentFarbe = "rgb(255, 255, 255)";
+  
+  constructor(private randomizer: Randomizer, private callingPokeApi: CallingPokeapi) { };
+  
 
   async ngOnInit() {
     await this.getRandomNationalDexPokemon();
@@ -28,6 +32,66 @@ export class RandomCard implements OnInit {
     if (this.pokemon.name == "") {
       console.error("Hey, hier wurde kein richtiges Pokemon rausgegeben! :(");
     }
+    this.setzeFarben();
+    
     this.datenSignal.set(this.pokemon);
+    
+  }
+
+  private setzeFarben() {
+    switch (this.pokemon.farbe) {
+      case "green":
+        this.pokemon.farbe = "rgb(33, 148, 73)";
+        this.akzentFarbe = "rgb(159, 242, 188)";
+        break;
+
+      case "white":
+        this.pokemon.farbe = "rgba(197, 197, 197, 1)";
+        this.akzentFarbe = "rgb(255, 255, 255)";
+        break;
+
+      case "brown":
+        this.pokemon.farbe = "rgb(125, 80, 0)";
+        this.akzentFarbe = "rgb(242, 205, 140)";
+        break;
+
+      case "yellow":
+        this.pokemon.farbe = "rgb(237, 169, 0)";
+        this.akzentFarbe = "rgb(255, 225, 149)";
+        break;
+
+      case "red":
+        this.pokemon.farbe = "rgb(181, 34, 0)";
+        this.akzentFarbe = "rgb(255, 174, 156)";
+        break;
+
+      case "blue":
+        this.pokemon.farbe = "rgb(19, 101, 179)";
+        this.akzentFarbe = "rgb(127, 192, 252)";
+        break;
+
+      case "pink":
+        this.pokemon.farbe = "rgb(191, 81, 173)";
+        this.akzentFarbe = "rgb(255, 208, 247)";
+        break;
+
+      case "purple":
+        this.pokemon.farbe = "rgb(104, 24, 186)";
+        this.akzentFarbe = "rgb(197, 138, 255)";
+        break;
+
+      case "gray":
+        this.pokemon.farbe = "rgba(85, 85, 85, 1)";
+        this.akzentFarbe = "rgba(197, 197, 197, 1)";
+        break;
+
+      case "black":
+        this.pokemon.farbe = "rgb(42, 42, 42)";
+        this.akzentFarbe = "rgba(197, 197, 197, 1)";
+        break;
+
+      default:
+        break;
+    }
   }
 }
